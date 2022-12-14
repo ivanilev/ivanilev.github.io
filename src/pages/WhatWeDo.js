ac9e561800c317b261b4b7d2406b9f555a29cbb1
georgievmv <96878439+georgievmv@users.noreply.github.com>
2022-12-14 15:46:21 +0200

    adding what we do paragraph

--- src/pages/WhatWeDo.js
+++ src/pages/WhatWeDo.js
@@ -6,6 +6,14 @@ const WhatWeDo = () => {
     <div id="WhatWeDo" className={styles.container}>
       <div className={styles.frame}>
         <h1>What we do?</h1>
+        <p className={styles.paragraph}>
+          No Language Barriers puts an emphasis on high quality. In order to
+          guarantee such we have one translator, usually a native speaker of the
+          target language, to translate the content, and then another one to do
+          a proofreading of his or her work. The two linguists then discuss and
+          fix all differences in translation before delivering the final product
+          to the client.
+        </p>
         <div className={styles.image}></div>
       </div>
     </div>
