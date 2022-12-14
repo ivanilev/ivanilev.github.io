072cd21c60a5fc99315324357bd669d9b2b67968
georgievmv <96878439+georgievmv@users.noreply.github.com>
2022-12-15 00:33:43 +0200

    updating whatwedo paragraph

--- src/pages/WhatWeDo.js
+++ src/pages/WhatWeDo.js
@@ -8,11 +8,14 @@ const WhatWeDo = () => {
         <h1>What we do?</h1>
         <p className={styles.paragraph}>
           No Language Barriers puts an emphasis on high quality. In order to
-          guarantee such we have one translator, usually a native speaker of the
-          target language, to translate the content, and then another one to do
-          a proofreading of his or her work. The two linguists then discuss and
-          fix all differences in translation before delivering the final product
-          to the client.
+          guarantee that, we forward all queries to two team members, who are
+          native speakers of the target language requested by the client.
+        </p>
+        <p className={styles.paragraph}>
+          The first one would translate the content and the second one would do
+          a peer-review before sending it back to the first translator. Finally,
+          the two linguists will sit down, discuss and resolve all differences
+          before delivering the final product back to the client.
         </p>
         <div className={styles.image}></div>
       </div>
