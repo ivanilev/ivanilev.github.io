76f66712899a5e12611b7f9d7e4d881101cb6f8b
georgievmv <96878439+georgievmv@users.noreply.github.com>
2022-12-21 19:54:35 +0200

    style update on language change menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,15 @@
 import React from "react";
+import bgFlagEmoji from "../assets/bg-flag-emoji.png";
+import enFlagEmoji from "../assets/en-flag-emoji.png";
 import styles from "./Nav.module.css";
 import { useState, useEffect, useRef } from "react";
 import logo from "../assets/logo151.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faNavicon, faClose } from "@fortawesome/free-solid-svg-icons";
+import {
+  faNavicon,
+  faClose,
+  faCaretDown,
+} from "@fortawesome/free-solid-svg-icons";
 import { withNamespaces } from "react-i18next";
 import i18n from "../i18n";
 import {
@@ -16,7 +22,7 @@ const Nav = (props) => {
   const { t } = props;
   const navRef = useRef();
   const [isDesktop, setDesktop] = useState(window.innerWidth > 650);
-  const [selectedLanguage, setSelectedLanguage] = useState("BG");
+  const [selectedLanguage, setSelectedLanguage] = useState(bgFlagEmoji);
   const [dropDown, setDropDown] = useState(false);
 
   const languageChangeDropHandler = () => {
@@ -25,8 +31,8 @@ const Nav = (props) => {
 
   const languageChangeHandler = (e) => {
     i18n.changeLanguage(e.target.id);
-    let language = document.querySelector("#" + e.target.id);
-    setSelectedLanguage(language.firstChild.innerHTML);
+    let language = document.getElementById(e.target.id);
+    setSelectedLanguage(language.firstChild.src);
   };
 
   const closeIcon = (
@@ -46,7 +52,7 @@ const Nav = (props) => {
       />
     </div>
   );
-
+  const downIcon = <FontAwesomeIcon icon={faCaretDown}></FontAwesomeIcon>;
   const updateMedia = () => {
     setDesktop(window.innerWidth > 576);
   };
@@ -107,7 +113,14 @@ const Nav = (props) => {
             onClick={languageChangeDropHandler}
             className={styles.customSelect}
           >
-            <span className={styles.currentlySelected}>{selectedLanguage}</span>
+            <div className={styles.currentlySelectedContainer}>
+              <img
+                className={styles.currentlySelected}
+                src={selectedLanguage}
+                alt=""
+              />
+              {downIcon}
+            </div>
             <div
               style={dropDown ? { display: "block" } : {}}
               className={styles.dropDown}
@@ -117,7 +130,12 @@ const Nav = (props) => {
                 id="bg"
                 className={styles.dropDownItem}
               >
-                <span id="bg">BG</span>
+                <img
+                  className={styles.flagEmoji}
+                  id="bg"
+                  src={bgFlagEmoji}
+                  alt=""
+                />
                 <p id="bg">Bulgarian</p>
               </div>
               <div
@@ -125,7 +143,7 @@ const Nav = (props) => {
                 id="en"
                 className={styles.dropDownItem}
               >
-                <span id="en">EN</span>
+                <img className={styles.flagEmoji} src={enFlagEmoji} alt="" />
                 <p id="en">English</p>
               </div>
             </div>
