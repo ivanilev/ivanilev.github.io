e03d17e81de73444c5cf37ad1ea4780070322d6c
Ivan <ivanilev95@gmail.com>
2023-01-03 13:14:41 +0200

    updates ES flag

--- src/languages/languages.js
+++ src/languages/languages.js
@@ -3,6 +3,7 @@ import enFlagEmoji from "../assets/en-flag-emoji.png";
 import deFlagEmoji from "../assets/de-flag-emoji.png";
 import ptFlagEmoji from "../assets/pt-flag-emoji.png";
 import ruFlagEmoji from "../assets/ru-flag-emoji.png";
+import esFlagEmoji from "../assets/es-flag-emoji.png";
 
 export const languages = [
   { language: "English", languageKey: "en", image: enFlagEmoji },
@@ -10,5 +11,5 @@ export const languages = [
   { language: "Deutsch", languageKey: "de", image: deFlagEmoji },
   { language: "Português", languageKey: "pt", image: ptFlagEmoji },
   { language: "Русский", languageKey: "ru", image: ruFlagEmoji },
-  { language: "Español", languageKey: "es", image: ruFlagEmoji },
+  { language: "Español", languageKey: "es", image: esFlagEmoji },
 ];
